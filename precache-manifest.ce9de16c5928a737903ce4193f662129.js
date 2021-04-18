8de09ecbf6f249e487385056582f2d22a5a37d55
Argen <dui.argesh96@gmail.com>
2021-04-18 19:21:05 +0300

    Updates

---
+++ precache-manifest.ce9de16c5928a737903ce4193f662129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d582aa26f5e3d681d38f716109f7a2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "0154edfdda7f0227b2d6",
+    "url": "/my-app/static/css/main.5b94738b.chunk.css"
+  },
+  {
+    "revision": "5960e7b0e64ef3efa2b2",
+    "url": "/my-app/static/js/2.62368e13.chunk.js"
+  },
+  {
+    "revision": "0154edfdda7f0227b2d6",
+    "url": "/my-app/static/js/main.6162549f.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
